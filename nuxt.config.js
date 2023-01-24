c16b82a379bea9dbf117a40a513a45527c7d70bc
Minseo-dev <kms12782@gmail.com>
2023-01-24 16:19:30 +0900

    Change Tab Title & Tab Icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'test-nuxt',
+    title: 'Foodi:e',
     htmlAttrs: {
       lang: 'en',
     },
@@ -11,7 +11,7 @@ export default {
       {hid: 'description', name: 'description', content: ''},
       {name: 'format-detection', content: 'telephone=no'},
     ],
-    link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}],
+    link: [{rel: 'icon', type: 'image/x-icon', href: '/header/nav-logo.png'}],
     script: [
       {
         src: 'https://developers.kakao.com/sdk/js/kakao.js'
